2784836f75e74ae4dad81fe4424267347ffde45d
Brannon Dorsey <brannon@brannondorsey.com>
2017-10-29 15:03:49 -0500

    Use curl instead of wget

--- download_data.sh
+++ download_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget http://www-nlp.stanford.edu/data/glove.6B.zip
-unzip glove.6B.zip -d data/glove
-rm glove.6B.zip
+curl -L http://www-nlp.stanford.edu/data/glove.6B.zip -o data/glove.6B.zip
+unzip data/glove.6B.zip -d data/glove
+rm data/glove.6B.zip
